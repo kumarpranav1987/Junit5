e49357284839e4c948a866b514a7be50a1e5f229
Pranav Kumar <pranav.kumar@iongroup.com>
2020-03-02 11:26:40 +0530

    First Commit

---
+++ src/test/java/com/livetechstudy/Junit5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.livetechstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Junit5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
